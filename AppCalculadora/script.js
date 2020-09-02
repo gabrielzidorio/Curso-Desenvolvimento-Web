983f2f79b5895c6be2d2b7ebcb4e585b5620a0be
Gabriel Zidorio <gabrielzidorio@gmail.com>
2020-09-02 13:47:53 -0300

    Comentando o codigo

--- AppCalculadora/script.js
+++ AppCalculadora/script.js
@@ -1,14 +1,14 @@
 function calcular(tipo, valor) {
-    if (tipo === 'acao') {
+    if (tipo === 'acao') { //VERIFICA SE É UMA OPERADOR OU UM NÚMERO
         if (valor === 'c') {
              //LIMPAR DISPLAY
              document.getElementById('display').value = '';
         }
 
-        if (valor === '+' || valor === '-' || valor === '*' || valor === '/') {
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/') { //VERIRICA SE UM OPERADOR FOI CLICADO
             document.getElementById('display').value += valor;
-        } else if (valor === '=') {
-            document.getElementById('display').value = eval(document.getElementById('display').value);
+        } else if (valor === '=') { //VERIFICA SE O 'IGUAL' FOI CLICADO
+            document.getElementById('display').value = eval(document.getElementById('display').value); //SETA O RESULTADO DA OPERAÇÃO ARITMÉTICA NO DISPLAY
         }
         
     } else {
